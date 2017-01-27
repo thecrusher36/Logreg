69bb5369090f60637373153e391f5b3a51301153
Sandi Suryadi <thecrusher36@gmail.com>
2017-01-27 16:52:22 +0700

    ganti nama folder

--- Files on Web/Include/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Database config variables
-define("DB_HOST", "venus.unlimitcloud.com");
-define("DB_USER", "suryadi_admin");
-define("DB_PASSWORD", "13ss36SS");
-define("DB_DATABASE", "suryadi_logreg");
-?>
